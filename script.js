4a71f569ba2c9cc2ee654dc00302c0f95302b774
Denis <Kaktusryak@users.noreply.github.com>
2020-12-15 13:41:20 +0200

    laba_5_canvas_only

--- script.js
+++ script.js
@@ -1,159 +1,135 @@
-function task2(){
-    document.addEventListener('keypress', (event) => {
-        var keyName = event.key;
+let centre_block=document.querySelector("fifth").innerHTML;
+let workspace=document.querySelector("fifth");
+let form=document.querySelector(".event_log");
 
-        if (event.code == 'KeyZ'){
-            setTimeout(function(){
-                document.querySelector("#First").style.fontStyle='italic';
-                document.querySelector("#Sixth").style.fontStyle='italic';
 
-            },3000);
-            
+
+
+
+
+
+
+function add_to_local_storage(information)
+{
+    let existing=localStorage.getItem("event");
+    existing = existing ? existing.split(',') : [];
+    let time = new Date(Date.now());
+    
+    existing.push("<br>"+information+" time:"+ time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
+
+    
+    localStorage.setItem('event', existing.toString());
+}
+
+
+function building_canvas()
+{
+    document.querySelector(".play_button").style.display="none";
+    form.style.display="none";
+    localStorage.clear();
+    add_to_local_storage("animation started!");
+    workspace.innerHTML="";
+    let canvas=document.createElement("canvas"); 
+    canvas.className="canvas";
+    workspace.appendChild(canvas);
+    canvas.width=workspace.clientWidth;
+    canvas.height=workspace.clientHeight;
+    
+    let i = 0;
+    let j = 0;
+    let x1=canvas.width/2,
+        y1=canvas.height/2;
+       
+  
+    let ctx = canvas.getContext('2d');
+       
+   let a= setInterval( ()=>{
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        ctx.beginPath();
+        ctx.fillStyle = "green";
+        ctx.arc(x1, y1, 15, 0, 2 * Math.PI, true);
+        ctx.fill();
+        
+
+        if(x1 == canvas.width+50|| x1 ==-50 || y1 == -50 || y1 == canvas.height+50){
+            clearInterval(a);
+            add_to_local_storage("Circle touched border: "+j+ " moves")
+            stopping();
         }
-        setInterval(function(){if(document.querySelector("#Fifth").style.fontStyle=='italic' ){
-            
-            document.querySelector("#Fifth").style.fontStyle='normal';
-            
+
+
+        if(i == 0 ){
+            if(Math.abs(Math.abs(x1)-(canvas.width/2))==j){
+                i=1;
+                j++;
+                add_to_local_storage("GO LEFT");
+            }
+            else{
+            x1-=1;           
+            }
         }
         else{
-            
-            document.querySelector("#Fifth").style.fontStyle='italic';
-                
+            if(i ==1 ){
+                if(Math.abs(Math.abs(y1)-(canvas.height/2))==j){
+                    i=2;
+                    j++;
+                    add_to_local_storage("GO DOWN");
+                }
+                else{
+                y1+=1;
+                }
             }
-        },3000)
-            
+            else{
+                if(i == 2){
+                    if(Math.abs(Math.abs(x1)-(canvas.width/2))==j){
+                        i=3;
+                        j++;
+                        add_to_local_storage("GO RIGHT");
+                    }
+                    else{
+                    x1+=1; 
+                    }
+                }
+                else{
+                    if(i = 3){
+                        if(Math.abs(Math.abs(y1)-(canvas.height/2))==j){
+                            i=0;
+                            j++;
+                            add_to_local_storage("GO UP"  );
+                        }
+                        else{
+                        y1-=1;
+                        }
+                    }
+                }       
+            }
+        }
         
         
-    });
-}
+        
+        
+        
+    }, 0.01);
 
-function task1(){
-    let one = document.querySelector("first").innerHTML;
-    let two = document.querySelector("second").innerHTML;
-    let three = document.querySelector("third").innerHTML;
-    let four = document.querySelector("fourth").innerHTML;
-    let five = document.querySelector("fifth").innerHTML;
-    let six = document.querySelector("sixth").innerHTML;
+    let stop=document.querySelector(".stop");
+    stop.style.display="block";
+   
     
-    setTimeout(function(){
-        document.querySelector("fifth").innerHTML = six;
-        setTimeout(function(){
-            document.querySelector("fourth").innerHTML = five;
-            setTimeout(function(){
-                document.querySelector("third").innerHTML = four;
-                setTimeout(function(){
-                    document.querySelector("second").innerHTML = three;
-                    setTimeout(function(){
-                        document.querySelector("first").innerHTML = two;
-                        setTimeout(function(){
-                            document.querySelector("sixth").innerHTML = one;
-                        }, 5000);
-                    }, 5000);
-                }, 5000);
-            }, 5000);
-        }, 5000);
-    }, 5000);   
+    document.querySelector("third").appendChild(stop); 
+
+};
+
+
+function stopping(){
+    document.querySelector(".stop").style.display="none";
+    workspace.innerHTML=centre_block;
+    add_to_local_storage("animation stopped");
+    workspace.className=("centreblock");
+    workspace.style.display="grid";
+    form.style.display="block";
+    form.innerHTML=localStorage.getItem("event");
+    document.querySelector(".play_button").style.display="block";
 }
 
-function Get_Commits(url)
-{
-    fetch(url).then((response) => 
-    {
-        if (!response.ok)
-        {
-            Git_Resp_1.style.background = "red";
-            Git_Resp_1.style.textAling = "center";
-            Git_Resp_1.style.height = "50px";
-            Git_Resp_1.style.border = "2 px solid black";
-            Git_Resp_1.innerHTML = response.status + "-" + response.statusText;
-            throw new Error(response.message);
-        }
-        return response.json();
-    })
-    .then((contents) => 
-    {
-        let answ = "";
-        contents.forEach((i) => {
-            answ += `${i.commit.author.name} : ${i.commit.message}\n`;
-        });
-        Git_Resp_1.style.background = "green";
-        Git_Resp_1.style.textAling = "center";
-        Git_Resp_1.style.height = "50px";
-        Git_Resp_1.style.border = "2 px solid black";
-        Git_Resp_1.innerHTML = answ;
-        throw new Error(response.message);
-    })
-    .catch((Error) => {});
-}
-function task3(){
-    B1.onclick = () =>
-    {
-        let url = "https://api.github.com/repos/" + area_1.value + "/" + area_2.value + "/commits";
-        Get_Commits(url);
-    }   
-}
-
-function call_1(call_2, call_3){
-    console.log("One");
-    call_2(call_3);
-    console.log("Six");
-}
-function call_2(call_3){
-    console.log("Two");
-    console.log("Three");
-    call_3();
-}
-function call_3(){
-    console.log("Four");
-    console.log("Five")
-}
-function task4(call_1, call_2, call_3){
-    call_1(call_2, call_3);
-}
-
-function insertionSort(inputArr) {
-    let n = inputArr.length;
-        for (let i = 1; i < n; i++) {
-            
-            let current = inputArr[i];
-            
-            let j = i-1; 
-            while ((j > -1) && (current < inputArr[j])) {
-                inputArr[j+1] = inputArr[j];
-                j--;
-            }
-            inputArr[j+1] = current;
-        }
-    return inputArr;
-}
-function Save(){
-    const reg = /^\d+$/;
-    let Data = document.Form.Line.value;
-
-    let a = new Array();
-    a = Data.split(' ');
-    for(let j =0; j < a.length;j++){
-        for(let i = 0; i < a.length; i++){
-            if(!reg.test(a[i])){
-                a.splice(i,1);
-                break;
-            }
-        }
-    }   
-
-    insertionSort(a);
-
-    alert(a);
-}
-
-
-
-//////////////////
-//task1();
-task2();
-task3();
-task4(call_1, call_2,call_3);
-
-
-//Kaktusryak
-//kaktusryak.github.io
+    
